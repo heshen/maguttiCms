57529fbe7501bcc3e394e32a8c9ce88926a96d53
Marco Asperti <marco.a@gfstudio.com>
2017-02-07 11:57:25 +0100

    Fix File upload name bugs

--- app/maguttiCms/Admin/Controllers/AdminPagesController.php
+++ app/maguttiCms/Admin/Controllers/AdminPagesController.php
@@ -268,7 +268,7 @@ class AdminPagesController extends Controller
     private function mediaHandler($model,$media)
     {
         $UM = new UploadManager;
-        $model->$media = $UM->init($media,$this->request)->store()->getFileFullName();
+        $model->$media = ($UM->init($media,$this->request)->store()->getFileFullName()) ? :  $model->$media;
     }
 
     /**
