65ce79575a5939d3b098a46962aaa2641d3802aa
Marco Asperti <marco.a@gfstudio.com>
2017-03-27 14:06:04 +0200

    Add  get_ip  method

--- app/maguttiCms/Tools/Tool.class.php
+++ app/maguttiCms/Tools/Tool.class.php
@@ -276,4 +276,44 @@ class Tool {
 		$lang=(ma_not_null($_REQUEST['lang']))?$_REQUEST['lang']:$_SESSION['langSite'];
 		return $lang;
 	}
+
+
+    function get_ip()  {
+        $ip = FALSE;
+        // If HTTP_CLIENT_IP is set, then give it priority
+        if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
+            $ip = $_SERVER["HTTP_CLIENT_IP"];
+        }
+
+
+        // User is behind a proxy and check that we discard RFC1918 IP addresses
+        // if they are behind a proxy then only figure out which IP belongs to the
+        // user.  Might not need any more hackin if there is a squid reverse proxy
+        // infront of apache.
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+
+            // Put the IP's into an array which we shall work with shortly.
+            $ips = explode (", ", $_SERVER['HTTP_X_FORWARDED_FOR']);
+            if ($ip) { array_unshift($ips, $ip); $ip = FALSE; }
+
+            for ($i = 0; $i < count($ips); $i++) {
+                // Skip RFC 1918 IP's 10.0.0.0/8, 172.16.0.0/12 and 192.168.0.0/16
+                if (!preg_match ("/^(10|172\.16|192\.168)\./i", $ips[$i])) {
+                    if (version_compare(phpversion(), "5.0.0", ">=")) {
+                        if (ip2long($ips[$i]) != false) {
+                            $ip = $ips[$i];
+                            break;
+                        }
+                    } else {
+                        if (ip2long($ips[$i]) != -1) {
+                            $ip = $ips[$i];
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        $Ip=($ip)? $ip : $_SERVER['REMOTE_ADDR'];
+        return $Ip;
+    }
 }	
